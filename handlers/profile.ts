7e647d17a763ea4735b7df99d5c366bc0748bcce
Wil Wade <wil.wade@unfinished.com>
2023-07-06 16:30:02 -0400

    GetProfile working

--- handlers/profile.ts
+++ handlers/profile.ts
@@ -1,15 +1,37 @@
 import { Handler } from "openapi-backend";
 import type * as T from "../types/openapi";
+import { getApi } from "../services/frequency";
 
-export const getProfile: Handler<{}> = async (c, req, res) => {
+export const getProfile: Handler<{}> = async (c, _req, res) => {
   // T.Paths.GetProfile.PathParameters
-  const response: T.Paths.GetProfile.Responses.$200 = {
-    fromId: "123",
-    contentHash: "0xabcd",
-    content: "",
-    timestamp: new Date().toISOString(),
-  };
-  return res.status(200).json(response);
+  const msaId = c.request.params.dsnpId;
+
+  if (typeof msaId !== "string") {
+    return res.status(404).send();
+  }
+
+  try {
+    const api = await getApi();
+
+    let displayHandle = "";
+    const handle = await api.rpc.handles.getHandleForMsa(msaId);
+    if (handle.isSome) {
+      displayHandle = `${handle.value.base_handle}.${handle.value.suffix}`
+    }
+
+
+    const response: T.Paths.GetProfile.Responses.$200 = {
+      fromId: msaId,
+      contentHash: "",
+      content: "",
+      timestamp: new Date().toISOString(),
+      displayHandle,
+    };
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send();
+  }
 };
 
 export const createProfile: Handler<T.Paths.CreateProfile.RequestBody> = async (
